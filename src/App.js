d6588ac1ce5c8d2ac2156e7bb914b2a0ccacf10f
Chris Martine <cmmartine@gmail.com>
2023-05-05 14:03:40 -0400

    Task adding to task array and DOM

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ class App extends Component{
   constructor(props) {
     super(props);
     this.state = { tasks: ['Task one', 'Task Two'] };
+    this.addTask=this.addTask.bind(this);
+  }
+
+  addTask() {
+    let inputText = document.getElementById('task-input');
+    this.setState({tasks: [...this.state.tasks, inputText.value] });
+    inputText.value = '';
   }
 
   render() {
     return (
       <>
-        <input type="text" className="task-input" />
-        <input type="submit" className="task-submit" value="Create Task" />
+        <input type="text" className="task-input" id="task-input"/>
+        <input type="submit" className="task-submit" value="Create Task" onClick={this.addTask} />
         <ShowTasks tasks={this.state.tasks} />
       </>
     );
